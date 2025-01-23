7163037095b1dbe782645b253895b0b507aa5f22
Edward Espinoza <edw2503@gmail.com>
2025-01-23 12:17:15 -0500

    cambio de prueba para el CICD

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <h1 class="text-2xl font-bold">Bienvenido al Home de Arcux</h1>
+    <p class="text-lg">"En construccion"</p>
     <h1>{{ $t("app_title", { name: "Edward" }) }}</h1>
   </div>
 </template>
